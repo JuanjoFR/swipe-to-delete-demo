fc23732372c20008e8ec8f23d2e16d97c313b9e5
Juanjo Fernández <juanjo.fernandez@haa.st>
2022-03-09 18:13:12 +0100

    Fix inline style

--- libraries/application.tsx
+++ libraries/application.tsx
@@ -1,16 +1,21 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { ThemeProvider } from "@shopify/restyle";
 import * as React from "react";
+import { StyleSheet } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import ApplicationNavigator from "../navigators/application";
 import theme from "./theme";
 
+const styles = StyleSheet.create({
+  gestureHandler: { flex: 1 }
+});
+
 function Application(): JSX.Element {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        <GestureHandlerRootView style={{ flex: 1 }}>
+        <GestureHandlerRootView style={styles.gestureHandler}>
           <NavigationContainer>
             <ApplicationNavigator />
           </NavigationContainer>
